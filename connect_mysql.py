2bafa94cb3ac87d8c8eeb8054ec09ff1eaef945d
Gustavo Rosa <gth.rosa@uol.com.br>
2020-08-31 11:36:48 -0300

    Adding a MySQL connection class.

---
+++ connect_mysql.py
@@ -0,0 +1,17 @@
+from utils.database import Session
+
+# Creates a MySQL session
+db = Session()
+
+# Defines a query
+query = 'SELECT * from qeztn_normas_area'
+
+# Executes the query
+db.cursor.execute(query)
+
+# Iterates over the query results
+for row in db.cursor:
+    print(row)
+
+# Closes the MySQL session
+db.close()
---
+++ utils/database.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+
+class Session:
+    """Provides an abstraction that connects a MySQL session.
+
+    """
+
+    def __init__(self, host='127.0.0.1', port=3306, user='localhost', password='root', database='roncarati'):
+        """Initialization method.
+
+        Args:
+            host (str): Connection host.
+            port (int): Connection port.
+            user (str): MySQL username.
+            password (str): MySQL password.
+            database (str): Database to be connected.
+
+        """
+
+        # Connects to a MySQL server
+        self.session = mysql.connector.connect(host=host, port=port, user=user, 
+                                               password=password, database=database)
+
+        # Defines a cursor
+        self.cursor = self.session.cursor()
+
+    def close(self):
+        """Closes the MySQL connection.
+
+        """
+
+        # Closes the session
+        self.session.close()
