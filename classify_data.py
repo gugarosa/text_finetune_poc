de884271096685b919c5a0a14bf78d66b35f5c62
Gustavo Rosa <gth.rosa@uol.com.br>
2020-10-29 21:28:38 -0300

    Improving classification script.

--- classify_data.py
+++ classify_data.py
@@ -1,22 +1,24 @@
+from sklearn.model_selection import train_test_split
 from textfier.core import Dataset, Runner
 from textfier.tasks import SequenceClassificationTask
 
 import utils.data as d
 
 # Loads training and testing samples
-X_train, Y_train = d.load_csv('data/train.csv', delimiter='\\')
-X_test, Y_test = d.load_csv('data/test.csv', delimiter='\\')
+X, Y = d.load_csv('data/output.csv', delimiter='\\')
 
 # Creates labeled sentences from pre-loaded samples
-X_train_tok, Y_train_tok = d.create_labeled_sentences(X_train, Y_train)
-X_test_tok, Y_test_tok = d.create_labeled_sentences(X_test, Y_test)
+X_tok, Y_tok = d.create_labeled_sentences(X, Y)
+
+# Splitting data into training and testing labels
+X_train_tok, X_test_tok, Y_train_tok, Y_test_tok = train_test_split(X_tok, Y_tok, test_size=0.5, random_state=0)
 
 # Creates the sequence classification task with pre-trained model
-task = SequenceClassificationTask(model='neuralmind/bert-large-portuguese-cased', num_labels=2)
+task = SequenceClassificationTask(model='neuralmind/bert-large-portuguese-cased', num_labels=5)
 
 # Encodes the training and testing samples
-X_enc_train = task.tokenizer(X_train_tok, return_tensors='pt', padding=True, truncation=True)
-X_enc_test = task.tokenizer(X_test_tok, return_tensors='pt', padding=True, truncation=True)
+X_enc_train = task.tokenizer(X_train_tok, return_tensors='pt', padding=True, truncation=True, max_length=512)
+X_enc_test = task.tokenizer(X_test_tok, return_tensors='pt', padding=True, truncation=True, max_length=512)
 
 # Creates the datasets
 train_dataset = Dataset(input_ids=X_enc_train['input_ids'],
